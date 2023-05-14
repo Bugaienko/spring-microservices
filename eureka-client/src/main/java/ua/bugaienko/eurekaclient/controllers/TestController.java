4ac5affa285ba53b77cd5a38e6ba420353054186
Bugaienko <sergiibugaienko@gmail.com>
2023-05-14 16:03:39 +0200

    config-server added

--- eureka-client/src/main/java/ua/bugaienko/eurekaclient/controllers/TestController.java
+++ eureka-client/src/main/java/ua/bugaienko/eurekaclient/controllers/TestController.java
@@ -1,5 +1,6 @@
 package ua.bugaienko.eurekaclient.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,8 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/main")
 public class TestController {
 
+    @Value("${eureka.instance.instance-id}")
+    private String instanceId;
+
+
+    @Value("${tmpVar}")
+    private int tmpVar;
+
     @GetMapping("/test")
     public String test() {
-        return "test request";
+        return instanceId + " -> test request ->" + tmpVar;
     }
 }
